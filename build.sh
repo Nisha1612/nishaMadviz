8fdce7f1c014241a68031727f0b320e4cc361da5
Nisha1612 <122513092+Nisha1612@users.noreply.github.com>
2024-09-19 15:15:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic 
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
