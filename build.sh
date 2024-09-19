80347f3dea54b3a5be9abce6982b193d1ce5c7fa
IshitaGrover23 <122429946+IshitaGrover23@users.noreply.github.com>
2024-09-19 23:13:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,17 @@
 # exit on error
 set -o errexit
 
+# Ensure pip is installed and Python version is correct
+python3.10 -m venv venv
+source venv/bin/activate
+
+# Install the dependencies
 pip install -r requirements.txt
 
+# Collect static files for Django
 python manage.py collectstatic --no-input
 
+# Optionally create superuser if the environment variable is set
 if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input
 fi
