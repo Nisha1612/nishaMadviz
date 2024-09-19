70f6c8548009d013550036660af97215e550e668
Nisha1612 <122513092+Nisha1612@users.noreply.github.com>
2024-09-19 15:57:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+python manage.py collectstatic --no-input
+
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
 fi
